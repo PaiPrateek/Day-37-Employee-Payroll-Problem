2032ae5fa92a38769f57857ebd4f2aaca06e9f91
Prateek Pai <paiprateek123@gmail.com>
2022-07-31 12:02:50 +0530

    Welcome to Employee Payroll Problem

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Problem");
+        }
+    }
+}
